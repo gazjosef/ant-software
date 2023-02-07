55df582b3a9d687c58cc009c8a307bfd225f54bd
Gareth Hind <garethjhind@gmail.com>
2023-02-07 15:34:16 +1100

    Added Header

---
+++ client.js
@@ -0,0 +1,6 @@
+import sanityClient from "@sanity/client";
+
+export default sanityClient({
+  projectId: "ur2gecd7",
+  dataset: "production",
+});
---
+++ components/Layout/Header.jsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const Header = () => {
+  return <header className="header">Header</header>;
+};
+
+export default Header;
