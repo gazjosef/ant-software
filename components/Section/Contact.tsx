d61a7c107e6483619f05c7885828c63670a8427a
Gareth Hind <garethjhind@gmail.com>
2024-05-03 21:05:42 +1000

    Refactor: Rm Phone & Email Details From Contact Page

--- components/Section/Contact.tsx
+++ components/Section/Contact.tsx
@@ -18,24 +18,17 @@ const Contact: React.FC = () => {
 
       <div className="container">
         <section className="section-contact__details" ref={contactDetailsRef}>
-          <h1 className="primary-heading">
-            We are here to <span>help and answer</span> any questions you might
-            have.
-          </h1>
           <Image
             src="/img/illustrations/undraw_programming_2svr.svg"
             alt="responsive"
             width={300}
             height={225}
           />
-          <div className="phone | u-flex u-items-center u-justify-center u-gap-1">
-            <FaPhone size="3rem" color="hsl(202, 61%, 47%)" />
-            <p className="paragraph">0434 420 506</p>
-          </div>
-          <div className="email | u-flex u-items-center u-justify-center u-gap-1">
-            <FaEnvelope size="3rem" color="hsl(202, 61%, 47%)" />
-            <p className="paragraph">ant@antsoftware.dev</p>
-          </div>
+
+          <h1 className="primary-heading">
+            We are here to <span>help and answer</span> any questions you might
+            have.
+          </h1>
         </section>
         <section className="section-contact__form" ref={contactFormRef}>
           <EmailForm />
